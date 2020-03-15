0f24fe1fa3fe9dad3e8f3c3baa4397563435ed1c
pjotryk <pjotryk@ukr.net>
2020-03-15 22:43:19 +0200

    asdfjkl;

---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+/*jslint browser: true*/
+/*global $, jQuery, alert, console*/
+
+$(function () {
+  "use strict";
+  $(window).on("scroll", function () {
+    var introH = $("#intro").innerHeight();
+   
+   
+  });
+  if (window.console) {
+    console.log("your message");
+  }
+});
